e749c3f2f7fba0e4a271c69e2bacf4bb4d8fa2c3
lnanni <x-nani@hotmail.com>
2016-09-01 20:12:45 -0300

    1.4
    New Form

---
+++ ControlePecuarista/src/CadastrarPasto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ControlePecuarista.src
+{
+    public partial class CadastrarPasto : ControlePecuarista.Form1
+    {
+        public CadastrarPasto()
+        {
+            InitializeComponent();
+        }
+    }
+}
